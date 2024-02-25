28b1a144b6f0abe04fbb5b572f5df66efd813ac9
shahnazAK <shahnazahmed264@gmail.com>
2024-02-25 12:12:58 -0500

    initial

---
+++ src/hw1JavaBasic/MySelf.java
@@ -0,0 +1,15 @@
+package hw1JavaBasic;
+
+/*Project Name:Starts with upper case
+ * Package Name :Starts with lower case
+ * Class Name:Starts with upper case 
+ */
+public class MySelf {
+
+	public static void main(String[] args) {// Main method is the entry point java
+		System.out.println("Shahnaz Akter");
+		System.out.println("76 Merrell Avenue");
+		System.out.println("Stamford,Connecticut-06902");
+		System.out.println("USA");
+	}
+}
---
+++ src/hw2JavaVariablesInitialized/AboutMe.java
@@ -0,0 +1,34 @@
+package hw2JavaVariablesInitialized;
+
+public class AboutMe {
+
+	public String myCity;// Variable Declared
+
+//Now initializing string,INT,float,char and boolean type variables to define myself
+	public String myName = "Shahnaz";
+	public int myAge = 95;
+	public float myHeight = 5.42339f;
+	public char myGender = 'F';
+	public boolean fullTimeStudent = true;
+
+//Declare a default constructor
+	public AboutMe() {
+		System.out.println("......This is all about Myself......");
+	}
+
+//Implement a void type method
+	public void aboutMe() {
+		System.out.println("MyName:" + myName + "\nMy Age:" + myAge + "\nMy Height:" + myHeight + "\nMy Gender:"
+				+ myGender + "\nI am a full time student:" + fullTimeStudent);
+	}
+
+	public static void main(String[] args) {
+
+		AboutMe aboutMe = new AboutMe(); // instantiate AboutMe/default constructor initialized
+		aboutMe.aboutMe(); // Method initializes
+
+	}
+
+	// TODO Auto-generated method stub
+
+}
